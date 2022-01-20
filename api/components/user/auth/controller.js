221e5a5a7f4f12fad5ad6ecd7a3dd28beef84c4d
juli6464 <juli6464@hotmail.com>
2022-01-20 16:17:07 -0500

    first upload

---
+++ api/components/user/auth/controller.js
@@ -0,0 +1,23 @@
+const TABLA = 'auth';
+module.exports = function (injectedStore) {
+    let store = injectedStore;
+    if (!store) {
+        store = require('../../../store/dummy');
+    }
+
+    function upsert() {
+        const authData = {
+            id: data.id,
+        }
+        if (data.username) {
+            authData.username = data.username;
+        }
+        if (data.password) {
+            authData.password = data.password;
+        }
+        return store.upsert(TABLA, authData);
+    }
+    return {
+        upsert,
+    }
+};
---
+++ config.js
@@ -0,0 +1,21 @@
+module.exports = {
+    api: {
+        port: process.env.API_PORT || 3000,
+    },
+    post: {
+        port: process.env.POST_PORT || 3002,
+    },
+    jwt: {
+        secret: process.env.JWT_SECRET || 'notasecret!',
+    },
+    mysql: {
+        host: process.env.MYSQL_HOST || 'sql10.freemysqlhosting.net',
+        user: process.env.MYSQL_USER || 'sql10466743',
+        password: process.env.MYSQL_PASS || '6z9P9gcwva',
+        database: process.env.MYSQL_DB || 'sql10466743',
+    },
+    mysqlService: {
+        host: process.env.MYSQL_SRV_HOST || 'localhost',
+        port: process.env.MYSQL_SRV_PORT || 3001,
+    }
+}
